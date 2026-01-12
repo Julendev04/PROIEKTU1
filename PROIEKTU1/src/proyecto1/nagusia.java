0a6a7b98567919e0eb15071a94a51b2e4dc5b159
Julen Ruiz De Viñaspre <ruizdevinaspre4@gmail.com>
2026-01-12 14:01:23 +0100

    Lerro berria adar berrian

--- PROIEKTU1/src/proyecto1/nagusia.java
+++ PROIEKTU1/src/proyecto1/nagusia.java
@@ -3,6 +3,7 @@ package proyecto1;
 public class nagusia {
 	public static void main(String[] args) {
 		System.out.println("Hello word");
+		System.out.println("Lerro berria adar berrian");
 	}
 
 }
